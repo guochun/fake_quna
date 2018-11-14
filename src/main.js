3e1b22a4c6521d84eb35f866ed5d9a8327aa5588
Purcy Guo <purcyguo@gmail.com>
2018-11-15 04:20:58 +0800

    fake_quna project init

--- src/main.js
+++ src/main.js
@@ -3,7 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import fastClick from 'fastclick'
+
+import './assets/css/reset.css'
+import './assets/css/border.css'
+
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 new Vue({
   router,
